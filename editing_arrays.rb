7a33026697899b8c8efa564e5a29d6e4a29c6881
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-25 15:22:41 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,3 +11,7 @@ end
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 end
+#Then we run our tests, these colors are printed out in order using a Ruby gem,
+#_colorize_. Our goal is to change this array so that it becomes "red",
+#"light_red", "light_yellow", "green", and "blue".  When printed, this will get
+#us the closest to a completed rainbow we can get in our terminal.
